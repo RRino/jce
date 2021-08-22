cbee394b05e63d97ebff64e0fdd9c6577c84f5eb
Ryan <ryan@widgetfactorylimited.co.uk>
2021-08-22 20:13:41 +0100

    Remove editor_plugin.js files

--- components/com_jce/editor/tiny_mce/plugins/ui/editor_plugin.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    tinymce.create('tinymce.plugins.UiPlugin', {
-        init: function (ed, url) {}
-    });
-
-    // Register plugin
-    tinymce.PluginManager.add('ui', tinymce.plugins.UiPlugin);
-})();
